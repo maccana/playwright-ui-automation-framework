01fd8812cf203bde1be3a904791b47c3ef391760
“maccana” <mikeymccann@gmail.com>
2026-06-01 14:05:59 -0400

    chore: configure html test reporter and automatic failure screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,14 +22,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  // 1. Change this line to include both the terminal ('line') and the interactive HTML view
+  reporter: [['line'], ['html', { open: 'never' }]],
+  
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // 2. Add this line to automatically snap screenshots on failures
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
